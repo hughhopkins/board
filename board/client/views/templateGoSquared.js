85738b2e24f4b2058bbd36b9d42f216f18d4dc5c
Hugh Hopkins <hugh.hopkins@gmail.com>
2014-04-04 00:01:53 +0100

    adding GoSquared

---
+++ board/client/views/templateGoSquared.js
@@ -0,0 +1,13 @@
+// first, lets get the data using jQuery
+$.getJSON('https://api.gosquared.com/now/v3/pages?api_key=demo&site_token=GSN-106863-S&limit=10&callback=?', function(data) {
+  var html = '';
+
+  // loop over the list and add each one to html
+  for (var i = 0; i < data.list.length; i++) {
+    var page = data.list[i];
+    html += '<li><a href="' + page.href + '" title="' + page.title + '">' + page.title + '</a></li>';
+  }
+
+  // select your list (ul) and insert the HTML into there
+  $('#top-content-list').html(html);
+});
